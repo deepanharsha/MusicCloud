b0503843ed06e743d3b78c36643c391603491156
ShOeLaYz <87807647+ShOeLaYz@users.noreply.github.com>
2022-01-17 13:51:47 +0530

    Add files via upload
    Vid3_2k.mp4 Not Yet Uploaded Due To: 711MB Size Of Filel

---
+++ script.js
@@ -0,0 +1,23 @@
+function vid4() {
+    location.replace("vid4.htm");
+}
+
+function gb1() {
+    location.replace("index.html")
+}
+
+function vid3() {
+location.replace("vid3.htm")
+}
+function vid2() {
+    location.replace("vid2.htm")
+    }
+    function cp() {
+        alert("DataBase //cookiepolicy.htm Unavailable Try Again Later")
+        }
+        function playvid3() {
+            location.replace("Vid3_2k.mp4")
+        }
+        function download() {
+            location.replace("MusicCloud.zip")
+        }
